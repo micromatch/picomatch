9caca07ed8095e4aaface446c82f0cf8d42b2958
Jon Schlinkert <jon.schlinkert@sellside.com>
2019-10-31 02:12:14 -0400

    fixes https://github.com/micromatch/picomatch/issues/20

---
+++ test/options.noglobstar.js
@@ -0,0 +1,14 @@
+'use strict';
+
+require('mocha');
+const assert = require('assert').strict;
+const { isMatch } = require('..');
+
+describe('options.noglobstar', () => {
+  it('should disable extglob support when options.noglobstar is true', () => {
+    assert(isMatch('a/b/c', '**', { noglobstar: false }));
+    assert(!isMatch('a/b/c', '**', { noglobstar: true }));
+    assert(isMatch('a/b/c', 'a/**', { noglobstar: false }));
+    assert(!isMatch('a/b/c', 'a/**', { noglobstar: true }));
+  });
+});
