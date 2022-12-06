215ce55133c8280d0223302f71e890bd50f9a067
Matt Hargett <plaztiksyke@gmail.com>
2022-12-06 10:36:04 -0800

    Add license text and return export table

--- lib/constants.lua
+++ lib/constants.lua
@@ -1,3 +1,26 @@
+--[[
+The MIT License (MIT)
+
+Copyright (c) 2017-present, Jon Schlinkert.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+]]
 --!strict
 
 --local path = require('path')
@@ -82,7 +105,7 @@ local POSIX_REGEX_SOURCE = {
   xdigit = 'A-Fa-f0-9'
 }
 
-module.exports = {
+local exports = {
   MAX_LENGTH = 1024 * 64,
   POSIX_REGEX_SOURCE,
 
@@ -176,3 +199,5 @@ module.exports = {
     return if win32 == true then WINDOWS_CHARS else POSIX_CHARS
   end
 }
+
+return exports
