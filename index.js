f7751def5ceea7a294b0007aa8b4181784856cff
Jon Schlinkert <jon.schlinkert@sellside.com>
2024-03-27 23:22:20 -0400

    move `isWindows` to utils

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
-/*global navigator*/
-
 'use strict';
 
 const pico = require('./lib/picomatch');
-
-const isWindows = () => {
-  if (typeof navigator !== 'undefined' && navigator.platform) {
-    const platform = navigator.platform.toLowerCase();
-    return platform === 'win32' || platform === 'windows';
-  }
-
-  if (typeof process !== 'undefined' && process.platform) {
-    return process.platform === 'win32';
-  }
-
-  return false;
-};
+const utils = require('./lib/utils');
 
 function picomatch(glob, options, returnState = false) {
   // default to os.platform()
   if (options && (options.windows === null || options.windows === undefined)) {
     // don't mutate the original options object
-    options = { ...options, windows: isWindows() };
+    options = { ...options, windows: utils.isWindows() };
   }
 
   return pico(glob, options, returnState);
